ca7e7bfdd478651c9a738cb52c315460692d754d
Ben V <ben@bluefigweb.com>
2019-05-07 21:15:48 -0600

    initial project commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/** 	
+	TODO: 
+	** Starship sales site w/features:
+	 - list all starships
+	 - filter by price
+	 - sort by price 
+	 - search list of ships 
+	 - view individual ships 
+	 - view pilot info of ships
+	 
+	 API endpoints: http://swapi.co/api/starships & http://swapi.co/api/people/:id
+**/
