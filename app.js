b5879e6f7f73a77490ea546dbffaabe795b8f318
Ben V <ben@bluefigweb.com>
2019-05-08 21:50:18 -0600

    refactor data logic, pull in second endpoint for character info, add colors, formatting

--- app.js
+++ app.js
@@ -11,45 +11,55 @@
 	 API endpoints: http://swapi.co/api/starships & http://swapi.co/api/people/:id
 **/
 const fetch = require('node-fetch');
-const stdio = require('stdio');
 const colors = require('colors');
+const url = require('url');
 const program = require('commander');
 
-program.version('1.0.0')
-	   .option('-a, --all', 'show all starship info')
-	   .option('-l, --list', 'list all ships')
-	   .option('-p, --price', 'filter ships by price')
-       .option('-pl, --pilots', 'get pilot info')
-       //.option('-s, --sort', 'sort ships by price')
-       .parse(process.argv);
-console.log('Run -h or --help for all options'.cyan + '\r\n');
+program
+	.version(`1.0.0`.white)
+	.description(`A handy command line reference for Star Wars`.rainbow)
+	.option(`-a, --all`, `Show all raw starship info`)
+    .option(`-l, --list`, `List some ship tech specs`)
+	.option(`-p, --price`, `Filter ships by price`)
+    .option(`--pilots`, `List last operators of the ship`)
+    .parse(process.argv);
+    
+console.log(` In a computer terminal far far away... \n Run -h or --help for all options`.cyan);
 
 
-//stdio.question('Enter a ship name '.cyan, () => {
-function getStarships() {	
+function getStarships() {
 	fetch(`http://swapi.co/api/starships`).then((res) => {
 	    return res.json();
 	    }).then((starships) => {
 	      let output = starships.results;
+
 	      output.forEach((ship) => {
-		    if(program.all) console.log(ship);
-			if(program.list) console.log(ship.name.yellow + ': ' + ship.model.green + ' - ' + ship.manufacturer + '\r\n' + ship.pilots);
-			if(program.price) console.log(ship.name + ' - ' + ship.cost_in_credits.blue);
-			if(program.pilots) console.log(ship.pilots.toString());
-	      })
+					let pilotUrls = ship.pilots.join('\r\n');
+					let parsed = pilotUrls.split('/');
+					let id = parsed[5];
+
+					if(program.all) console.log(ship); 
+					if(program.price) console.log(`\n Ship: ${ship.name} \n Price: ${ship.cost_in_credits.blue}`);
+					
+
+					fetch(`https://swapi.co/api/people/${id}`).then((res) => {
+						return res.json();
+					}).then((person) => {
+						if(program.pilots && id !== undefined) { 
+							console.log(`\n Pilot: ${person.name.magenta} \n Ship: ${ship.name.yellow}`); 
+						} else if(program.pilots && id == undefined) {
+							console.log(`\n Pilot: No pilot found \n Ship: ${ship.name.yellow}`); 
+						}
+							
+						if(program.list && id !== undefined) {
+							console.log(`\n Ship: ${ship.name.yellow} \n Model: ${ship.model.grey} \n Cargo Capacity: ${ship.cargo_capacity.magenta} \n Manufacturer: ${ship.manufacturer.white} \n Pilots: ${person.name.cyan}`);
+						} else if(program.list && id == undefined) {
+							console.log(`\n Ship: ${ship.name.yellow} \n Model: ${ship.model.grey} \n Manufacturer: ${ship.manufacturer.white} \n Pilots: No pilots found`);
+						} 
+					})
+	     	});
 	    }).catch(error => console.error(`There was an ${error}`.red))
+	   
 }
 
 getStarships();
-
-function getPilot(pilot) {
-	fetch(`https://swapi.co/api/people/:1`).then((res) => {
-		return res.json();
-	}).then((person) => {
-		console.log(person.name);
-	})
-}
-
-//getPilot();
- 
-// });
